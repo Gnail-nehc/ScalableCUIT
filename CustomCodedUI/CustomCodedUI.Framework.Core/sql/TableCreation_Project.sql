f7343fa586cce7bcfec625d60bb82bc327ef5c1f
曦 <gnail1022@gmail.com>
2013-11-07 01:18:45 +0800

    Update TableCreation_Project.sql

--- CustomCodedUI/CustomCodedUI.Framework.Core/sql/TableCreation_Project.sql
+++ CustomCodedUI/CustomCodedUI.Framework.Core/sql/TableCreation_Project.sql
@@ -1,22 +1,20 @@
-USE [QA_Automation_Test_POC]
+USE [Automation]
 GO
 
-/****** Object:  Table [QA_Automation_Test].[Project]    Script Date: 03/01/2013 04:22:25 ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[QA_Automation_Test].[Project]') AND type in (N'U'))
-DROP TABLE [QA_Automation_Test].[Project]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Automation].[Project]') AND type in (N'U'))
+DROP TABLE [Automation].[Project]
 GO
 
-USE [QA_Automation_Test_POC]
+USE [Automation]
 GO
 
-/****** Object:  Table [QA_Automation_Test].[Project]    Script Date: 03/01/2013 04:22:25 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [QA_Automation_Test].[Project](
+CREATE TABLE [Automation].[Project](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[ProjectName] [varchar](50) NOT NULL,
  CONSTRAINT [PK_Project] PRIMARY KEY CLUSTERED 
